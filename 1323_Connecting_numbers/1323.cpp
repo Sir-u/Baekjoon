e13ec00bea28e9fdd9b4c52cca8e1183c01d4afd
Siru <ehdgur9553@naver.com>
2022-01-25 01:40:50 +0900

    01.25

--- 1323_Connecting_numbers/1323.cpp
+++ 1323_Connecting_numbers/1323.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  cout << "test plus\n";
-  cout << "the state is good\n";
-  int a;
-  cin >> a; 
-  cout << a;
+int reminder(int &a, int &b){
+  return a % b;
+}
+
+int division(int &a, int &b){
+  return a / b;
+}
+
+int main()
+{
+  int n = 0, k = 0, result = 0, nul = 0;
+  cin >> n >> k;
+  result = n % k;
+
+  if (result != 0){
+    
+  }else{
+    cout << 1;
+  }
+
+  nul = division(n, k); //test
+
+  cout << n << " "<<  k << " " << result << " "<< nul; //test
+  
 }
