0b494be98962f9cc8017f5273ad17552692e7fad
Siru <ehdgur9553@naver.com>
2022-01-26 01:17:31 +0900

    end

--- 1323_Connecting_numbers/1323.cpp
+++ 1323_Connecting_numbers/1323.cpp
@@ -1,28 +1,57 @@
 #include <iostream>
-using namespace std;
+#include <cmath>
+// using namespace std;
 
-int reminder(int &a, int &b){
-  return a % b;
+bool Check[100001] = {0};
+
+long long involution(int a, int b) {
+	if (b == 0) {
+		return 1;
+	}
+	else if (b == 1) {
+		return a;
+	}
+	long long tmp = involution(a, b / 2);
+	if (b % 2) {
+		return tmp * tmp * a;
+	}
+	else {
+		return tmp * tmp;
+	}
 }
 
-int division(int &a, int &b){
-  return a / b;
+int CheckCipher(int a)
+{
+  int cipher = 0;
+  while (a > 0)
+  {
+    cipher++;
+    a /= 10;
+  }
+  cipher = involution(10, cipher);
+  return cipher;
 }
 
 int main()
 {
-  int n = 0, k = 0, result = 0, nul = 0;
-  cin >> n >> k;
-  result = n % k;
+  int n = 0, k = 0, remind = 0;
+  std::cin >> n >> k;
+  long long cipher = CheckCipher(n); // n의 자리수
+  remind = n % k;                     //우선 나머지 계산
+  int cnt = 1;                        //카운트 초기값 1
+  while (remind != 0)                 //나누어 떨어지지 않을 경우 실행
+  {
+    Check[remind] = true;
+    long long m = remind * cipher + n;                                         // N의 연속숫자
+    remind = m % k;
 
-  if (result != 0){
-    
-  }else{
-    cout << 1;
+    if (Check[remind]) //나머지가 중복되어 사이클이 될 경우
+    {
+      cnt = -1;
+      break;
+    }
+    cnt++;
   }
 
-  nul = division(n, k); //test
-
-  cout << n << " "<<  k << " " << result << " "<< nul; //test
-  
+  std::cout << cnt;
 }
